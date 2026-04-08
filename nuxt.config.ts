e1f6099cbf47fd1504c7deb7a6002246fb708e57
phaladrien-ui <phaladrien@github.com>
2026-04-08 17:45:44 +0000

    upgrade

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,11 +6,14 @@ export default defineNuxtConfig({
     '@nuxtjs/mdc',
     '@nuxthub/core',
     'nuxt-auth-utils',
-    'nuxt-charts'
+    'nuxt-charts',
+    '@nuxtjs/google-fonts'
   ],
 
   runtimeConfig: {
     deepseekApiKey: process.env.DEEPSEEK_API_KEY,
+    tursoDatabaseUrl: process.env.TURSO_DATABASE_URL,
+    tursoAuthToken: process.env.TURSO_AUTH_TOKEN,
     public: {
       // Variables publiques ici si besoin
     }
@@ -55,5 +58,15 @@ export default defineNuxtConfig({
         braceStyle: '1tbs'
       }
     }
+  },
+
+  // @ts-ignore - Le module google-fonts est installé
+  googleFonts: {
+    families: {
+      Inter: [400, 500, 600, 700],
+      'JetBrains+Mono': [400]
+    },
+    display: 'swap',
+    preconnect: true
   }
 })
